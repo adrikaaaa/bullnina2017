0e7748f70308cfa2ab2ea4fbc657501ae8f36cd2
diether werk <diether1@gmail.com>
2016-10-03 00:26:27 -0300

    test

--- bullhalloween/settings/local.py
+++ bullhalloween/settings/local.py
@@ -14,10 +14,7 @@ COMPRESS_ENABLED = True
 
 PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_DIR, 'bullhalloween.db'),
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite')
 }
 
 
